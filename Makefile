ccde56e4bb0bb1e826c8a9ba6b46c0253e3eaa45
roman-makhov <roman.makhov@fin-frame.ru>
2026-04-09 23:46:06 +0300

    step_2

--- Makefile
+++ Makefile
@@ -1,6 +1,17 @@
-.PHONY: run-debug run-prod
+.PHONY: run-debug run-prod lint test build full-flow
 run-debug:
 	go run main.go
 
 run-prod:
 	GIN_MODE=release go run main.go
+
+lint:
+	golangci-lint run
+
+test:
+	go test ./...
+
+build:
+	go build
+
+full-flow: lint test build
--- main.go
+++ main.go
@@ -1,13 +1,42 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"errors"
+	"log"
 
-func main() {
+	"github.com/gin-gonic/gin"
+)
+
+func setupRouter() *gin.Engine {
 	router := gin.New()
 	router.Use(gin.Logger())
 	router.Use(gin.Recovery())
+	router.Use(errorHandler())
+
 	router.GET("/ping", func(c *gin.Context) {
+		c.Error(errors.New("[pong] something went wrong"))
 		c.String(200, "pong")
 	})
-	router.Run(":8080")
+
+	return router
+}
+
+func errorHandler() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Next()
+
+		if len(c.Errors) > 0 {
+			err := c.Errors.Last().Err
+
+			log.Println(err.Error())
+		}
+	}
+}
+
+func main() {
+	router := setupRouter()
+
+	if err := router.Run(":8080"); err != nil {
+		log.Fatal(err)
+	}
 }
